c0246a5277503dff291f05488af89b9fb76e8790
vanStaen <clement.vanstaen@gmail.com>
2023-07-09 18:42:43 +0200

    menu backgroufd color as prop

--- src/pages/Welcome/MyLinks/LinkElement.jsx
+++ src/pages/Welcome/MyLinks/LinkElement.jsx
@@ -4,10 +4,31 @@ import { observer } from "mobx-react";
 import "./LinkElement.less";
 
 export const LinkElement = observer((props) => {
+  const handleMouseEnter = (element) => {
+    const item = document.getElementById(element);
+    const isRadialGradient = props.color.substring(0, 15) === "radial-gradient";
+    if (isRadialGradient) {
+      item.style.background = props.color;
+    } else {
+      item.style.backgroundColor = props.color;
+    }
+  };
+
+  const handleMouseLeave = (element) => {
+    const item = document.getElementById(element);
+    item.style.background = "none";
+    item.style.backgroundColor = "white";
+  };
+
   const formatedSubMenu = props.links.map((link) => {
     return (
       <div className="subMenuItem">
-        <div className="item">
+        <div
+          className="item"
+          id={`item_${link.key}`}
+          onMouseEnter={() => handleMouseEnter(`item_${link.key}`)}
+          onMouseLeave={() => handleMouseLeave(`item_${link.key}`)}
+        >
           <a href={link.url} target="_blank" className="item">
             {link.account}
           </a>
--- src/pages/Welcome/MyLinks/MyLinks.jsx
+++ src/pages/Welcome/MyLinks/MyLinks.jsx
@@ -27,24 +27,30 @@ export const MyLinks = observer(() => {
       <LinkElement
         name="Instagram"
         logo={InstaLogo}
+        color="radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)"
         links={[
           {
+            key: 1,
             account: "@clementvanstaen",
             url: "https://www.instagram.com/clementvanstaen/",
           },
           {
+            key: 2,
             account: "@vanstaenmusic",
             url: "https://www.instagram.com/vanstaenmusic/",
           },
           {
+            key: 3,
             account: "@deuxfrancs",
             url: "https://www.instagram.com/deuxfrancs/",
           },
           {
+            key: 4,
             account: "@vinticiousberlin",
             url: "https://www.instagram.com/vinticiousberlin/",
           },
           {
+            key: 5,
             account: "@miralykkeofficial",
             url: "https://www.instagram.com/miralykkeofficial/",
           },
@@ -53,20 +59,25 @@ export const MyLinks = observer(() => {
       <LinkElement
         name="Spotify"
         logo={SpotifyLogo}
+        color="rgba(29, 185, 84, 1)"
         links={[
           {
+            key: 6,
             account: "van Staen",
             url: "https://open.spotify.com/intl-de/artist/0dsj1Ni4YlrtmcySPpnrhp",
           },
           {
+            key: 7,
             account: "Mira Lykke",
             url: "https://open.spotify.com/intl-de/artist/6RjHfJFz2WqJ4oGTAHRglg",
           },
           {
+            key: 8,
             account: "Wilhelm Harakiri",
             url: "https://open.spotify.com/intl-de/artist/4KLcngF6tGCMbB8fA2uWqn",
           },
           {
+            key: 9,
             account: "The Antiquariat",
             url: "https://open.spotify.com/intl-de/artist/5qGLaOu3xYCxklHmJuiFso",
           },
@@ -75,16 +86,20 @@ export const MyLinks = observer(() => {
       <LinkElement
         name="Soundcloud"
         logo={SoundcloudLogo}
+        color="rgba(242, 111, 35, 1)"
         links={[
           {
+            key: 10,
             account: "van Staen",
             url: "https://www.soundcloud.com/vanstaenmusic/",
           },
           {
+            key: 11,
             account: "Mira Lykke",
             url: "https://www.soundcloud.com/miralykke/",
           },
           {
+            key: 12,
             account: "Purzelbaum Records",
             url: "https://www.soundcloud.com/purzelbaumrecords/",
           },
@@ -93,16 +108,20 @@ export const MyLinks = observer(() => {
       <LinkElement
         name="Youtube"
         logo={YoutubeLogo}
+        color="rgba(255, 0, 0, 1)"
         links={[
           {
+            key: 13,
             account: "The Antiquariat",
             url: "https://www.youtube.com/@theantiquariat",
           },
           {
+            key: 14,
             account: "Purzelbaum Records",
             url: "https://www.youtube.com/@purzelbaumrecords3838",
           },
           {
+            key: 15,
             account: "Mute Bouldering Vlog",
             url: "https://www.youtube.com/@clementvanstaen",
           },
