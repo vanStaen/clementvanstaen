edc7ee8f997e79f356e3ac99d85d8cc7116a6383
vanStaen <clement.vanstaen@gmail.com>
2023-07-10 15:16:01 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ import {
 } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-import { Slider } from "./pages/Slider";
-import { Welcome } from "./pages/Welcome/Welcome";
+import { Main } from "./pages/Main";
 import { pageStore } from "./store/pageStore";
 
 import "../src/lib/i18n";
@@ -41,7 +40,7 @@ const App = observer(() => {
   }, [pageStore.language, i18n]);
 
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<Slider />}></Route>)
+    createRoutesFromElements(<Route path="/" element={<Main />}></Route>)
   );
 
   return (
