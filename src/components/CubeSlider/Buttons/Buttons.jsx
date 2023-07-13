d333fd4274e4f85d8b653341e0fda75a1bb70e7c
vanStaen <clement.vanstaen@gmail.com>
2023-07-13 19:56:40 +0200

    mobX observer on button

--- src/components/CubeSlider/Buttons/Buttons.jsx
+++ src/components/CubeSlider/Buttons/Buttons.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
+import { observer } from "mobx-react";
 
-import './Buttons.less';
+import "./Buttons.less";
 
-export const Buttons = () => {
+export const Buttons = observer(() => {
+  const handleControlClick = (elementId) => {
+    const element = document.getElementById(elementId);
+    element.classList.add("active");
+    setTimeout(() => {
+      element.classList.remove("active");
+    }, 500);
+  };
 
-    const handleControlClick = (elementId) => {
-        const element = document.getElementById(elementId);
-        element.classList.add("active");
-        setTimeout(() => { element.classList.remove("active") }, 500);
-    }
-
-    return (<>
-        <div className="controls">
-            <div className="next" onClick={() => handleControlClick('arrowRight')}>
-                <span id="arrowRight" className="arrowRight" />
-            </div>
-            <div className="prev" onClick={() => handleControlClick('arrowLeft')}>
-                <span id="arrowLeft" className="arrowLeft" />
-            </div>
+  return (
+    <>
+      <div className="controls">
+        <div className="next" onClick={() => handleControlClick("arrowRight")}>
+          <span id="arrowRight" className="arrowRight" />
         </div>
-    </>)
-}
+        <div className="prev" onClick={() => handleControlClick("arrowLeft")}>
+          <span id="arrowLeft" className="arrowLeft" />
+        </div>
+      </div>
+    </>
+  );
+});
