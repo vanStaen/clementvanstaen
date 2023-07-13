630c9254b94a435f6967d366eca1e4e5f723e40d
vanStaen <clement.vanstaen@gmail.com>
2023-07-13 08:20:51 +0200

    cubeSlider WIP

---
+++ src/components/CubeSlider/CubeSlider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CubeSlider = (props) => {
+  const { pages } = props;
+
+  //https://www.youtube.com/watch?v=SK9AlIbexOE
+
+  return <>{pages[0]}</>;
+};
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,32 +1,27 @@
 import React from "react";
-import AwesomeSlider from "react-awesome-slider";
-import coreStyles from "react-awesome-slider/src/core/styles.scss";
-import animationStyles from "react-awesome-slider/src/styled/cube-animation/cube-animation.scss";
+
+import { CubeSlider } from "../components/CubeSlider/CubeSlider";
 
 import { Welcome } from "./Welcome/Welcome";
 
 export const Main = () => {
+  const pages = [
+    <div key="welcomePage">
+      <Welcome />
+    </div>,
+    <div style={{ backgroundColor: "Coral" }} key="placeholder1">
+      placeholder 1
+    </div>,
+    <div style={{ backgroundColor: "DarkSlateGray" }} key="placeholder2">
+      placeholder 2
+    </div>,
+    <div style={{ backgroundColor: "Pink" }} key="placeholder3">
+      placeholder 3
+    </div>,
+  ];
   return (
     <>
-      <AwesomeSlider
-        animation="cubeAnimation"
-        cssModule={[coreStyles, animationStyles]}
-        fillParent={true}
-        bullets={false}
-      >
-        <div key="welcomePage">
-          <Welcome />
-        </div>
-        <div style={{ backgroundColor: "Coral" }} key="placeholder1">
-          placeholder 1
-        </div>
-        <div style={{ backgroundColor: "DarkSlateGray" }} key="placeholder2">
-          placeholder 2
-        </div>
-        <div style={{ backgroundColor: "Pink" }} key="placeholder3">
-          placeholder 3
-        </div>
-      </AwesomeSlider>
+      <CubeSlider pages={pages} />
     </>
   );
 };
