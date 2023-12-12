b55cf47f19ddf338be7b6fcd55a9c0763a3c1e8c
vanStaen <clement.vanstaen@gmail.com>
2023-12-12 21:21:19 +0100

    revolving text fix

--- src/components/RevolvingText/RevolvingText.jsx
+++ src/components/RevolvingText/RevolvingText.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { observer } from "mobx-react";
 
 import "./RevolvingText.less";
 
-export const RevolvingText = observer(() => {
+export const RevolvingText = () => {
   const [windowInnerHeight, setWindowInnerHeight] = useState(
     window.innerHeight
   );
@@ -21,67 +20,28 @@ export const RevolvingText = observer(() => {
     };
   }, [resetWindowInner]);
 
-  const words = document.getElementsByClassName("word");
-  /*const words = [
-    "Frontend Developer",
-    "Woodworker",
-    "Boulderer",
-    "Music producer",
-    "Photogrpaher",
-  ];*/
-  const wordArray = [];
-  var currentWord = 0;
-
-  function changeWord() {
-    var cw = wordArray[currentWord];
-    var nw =
-      currentWord == words.length - 1
-        ? wordArray[0]
-        : wordArray[currentWord + 1];
-
-    for (var i = 0; i < cw.length; i++) {
-      animateLetterOut(cw, i);
-    }
-
-    for (var i = 0; i < nw.length; i++) {
-      nw[i].className = "letter behind";
-      nw[0].parentElement.style.opacity = 1;
-      animateLetterIn(nw, i);
-    }
-    currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
-  }
-
-  function animateLetterOut(cw, i) {
-    setTimeout(function () {
-      cw[i].className = "letter out";
-    }, i * 80);
-  }
-
-  function animateLetterIn(nw, i) {
-    setTimeout(function () {
-      nw[i].className = "letter in";
-    }, 340 + i * 80);
-  }
-
-  function splitLetters(word) {
-    var content = word.innerHTML;
-    word.innerHTML = "";
-    var letters = [];
-    for (var i = 0; i < content.length; i++) {
-      var letter = document.createElement("span");
-      letter.className = "letter";
-      letter.innerHTML = content.charAt(i);
-      word.appendChild(letter);
-      letters.push(letter);
-    }
-    wordArray.push(letters);
-  }
+  let currentWord = 0;
+  let lastWord = 0;
+  let nextWord = 0;
 
   useEffect(() => {
+    const words = document.getElementsByClassName("word");
     words[currentWord].style.opacity = 1;
-    for (var i = 0; i < words.length; i++) {
-      splitLetters(words[i]);
-    }
+
+    const changeWord = () => {
+      words[currentWord].style.opacity = 0;
+      words[currentWord].className = "word out";
+      if (currentWord === 4) {
+        currentWord = 0;
+      } else {
+        currentWord = currentWord + 1;
+      }
+      words[currentWord].style.opacity = 1;
+      words[currentWord].className = "word behind";
+      setTimeout(function () {
+        words[currentWord].className = "word in";
+      }, 80);
+    };
     const refreshIntervalId = setInterval(changeWord, 3000);
     return () => {
       clearInterval(refreshIntervalId);
@@ -99,14 +59,24 @@ export const RevolvingText = observer(() => {
     >
       <div className="text">
         <p>I am a </p>&nbsp;
-        <p>
-          <span className="word">Programmer</span>
-          <span className="word">Musician</span>
-          <span className="word">Boulderer</span>
-          <span className="word">Woodworker</span>
-          <span className="word">Photographer</span>
+        <p className="words">
+          <span className="word" style={{ opacity: 1 }}>
+            Programmer
+          </span>
+          <span className="word" style={{ opacity: 0 }}>
+            Musician
+          </span>
+          <span className="word" style={{ opacity: 0 }}>
+            Boulderer
+          </span>
+          <span className="word" style={{ opacity: 0 }}>
+            Woodworker
+          </span>
+          <span className="word" style={{ opacity: 0 }}>
+            Photographer
+          </span>
         </p>
       </div>
     </div>
   );
-});
+};
