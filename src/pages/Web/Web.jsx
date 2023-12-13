e502b690386e7f92ba048d85b08ffa2f8d10f20a
vanStaen <clement.vanstaen@gmail.com>
2023-12-13 20:41:49 +0100

    web comming soon

--- src/pages/Web/Web.jsx
+++ src/pages/Web/Web.jsx
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react";
 
+import { isMobileCheck } from "../../helpers/checkMobileTablet";
+
 import rewaer_desktop_1 from "./img/rewaer_desktop_1.png";
 import rewaer_desktop_2 from "./img/rewaer_desktop_2.png";
 import rewaer_desktop_3 from "./img/rewaer_desktop_3.png";
+import rewaer_mobile_1 from "./img/rewaer_mobile_1.jpg";
+import rewaer_mobile_2 from "./img/rewaer_mobile_2.jpg";
+import rewaer_mobile_3 from "./img/rewaer_mobile_3.jpg";
+
+import kinkograph_desktop_1 from "./img/kinkograph_desktop_1.png";
+import kinkograph_desktop_2 from "./img/kinkograph_desktop_2.png";
+import kinkograph_desktop_3 from "./img/kinkograph_desktop_3.png";
+import kinkograph_mobile_1 from "./img/kinkograph_mobile_1.jpg";
+import kinkograph_mobile_2 from "./img/kinkograph_mobile_2.jpg";
+import kinkograph_mobile_3 from "./img/kinkograph_mobile_3.jpg";
+
+const THRESHOLD_IN_PX_BEFORE_SHOWING_MOBILE_GALLERY = 670;
 
 import "./Web.less";
 
@@ -10,9 +24,17 @@ export const Web = () => {
   const [windowInnerHeight, setWindowInnerHeight] = useState(
     window.innerHeight
   );
+  const [showMobileImages, setShowMobileImages] = useState(
+    isMobileCheck() ||
+      window.innerWidth < THRESHOLD_IN_PX_BEFORE_SHOWING_MOBILE_GALLERY
+  );
 
   const resetWindowInnerHeight = () => {
     setWindowInnerHeight(window.innerHeight);
+    setShowMobileImages(
+      isMobileCheck() ||
+        window.innerWidth < THRESHOLD_IN_PX_BEFORE_SHOWING_MOBILE_GALLERY
+    );
   };
 
   useEffect(() => {
@@ -25,6 +47,8 @@ export const Web = () => {
   return (
     <div className="web__container" style={{ minHeight: windowInnerHeight }}>
       <div className="title">Web Projects</div>
+      <div className="fourOFour">Comming soon</div>
+      {/*
       <div className="web__projectListContainer">
         <div className="web__projectList web__projectListSelected">
           <div
@@ -57,17 +81,17 @@ export const Web = () => {
       <div className="web__screenshots">
         <img
           className="web__screenshot web__screenshot1"
-          src={rewaer_desktop_1}
+          src={showMobileImages ? rewaer_mobile_1 : rewaer_desktop_1}
         />
         <img
           className="web__screenshot web__screenshot2"
-          src={rewaer_desktop_2}
+          src={showMobileImages ? rewaer_mobile_2 : rewaer_desktop_2}
         />
         <img
           className="web__screenshot web__screenshot3"
-          src={rewaer_desktop_3}
+          src={showMobileImages ? rewaer_mobile_3 : rewaer_desktop_3}
         />
-      </div>
+  </div>*/}
     </div>
   );
 };
